d7cb70a7b41a52d9c6f9594edaa3a316ab78845c
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2025-01-30 13:42:53 -0600

    Updating minimum iOS and macOS platforms. Swift 5.5 -> 5.7 tools.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "RoutingKitExtensions",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_15),
+        .iOS(.v13),
+        .macOS(.v12),
         .tvOS(.v15),
         .watchOS(.v8),
     ],
